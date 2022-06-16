677f16b86b411f8dc954f16342bcf67d773c8e39
AntoninB84 <73487440+AntoninB84@users.noreply.github.com>
2022-06-16 18:44:44 +0200

    Update Profile.js
    Confirmation désinscription

--- components/profile/profile.js
+++ components/profile/profile.js
@@ -1,6 +1,6 @@
-import React from 'react'
+import React, {useContext, useState} from 'react';
 import {connect, useSelector, useDispatch} from 'react-redux'
-import { View, Text, Button, StyleSheet } from 'react-native'
+import { View, Text, Button, StyleSheet, Modal, TouchableOpacity, ToastAndroid } from 'react-native'
 import Header from '../resources/header';
 
 import {deleteAccount} from '../../API/Auth.js'
@@ -8,6 +8,9 @@ import {deleteAccount} from '../../API/Auth.js'
 function Profile(props){
 
     const dispatch = useDispatch()
+    
+    
+    const [displayModale, setDisplayModale] = useState(false)
 
     const _disconnect = () => {
         const action = { type: "RESET", value: "" }
@@ -15,16 +18,45 @@ function Profile(props){
       }
     
       const _deleteAccount = () => {
-        deleteAccount(props.user_id, props.api_token).then(data => {
-          ToastAndroid.show(data.message, ToastAndroid.SHORT);
-          _disconnect()
-        })
+        setDisplayModale(true);
       }
+      
+      const deleteAccountModal = () => {
+      return (
+        <Modal
+          visible={displayModale}
+          transparent={true}
+          onRequestClose={()=>{
+            setDisplayModale(false)
+          }}
+        >
+          <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+              <Text>Etes-vous certains de vouloir supprimer votre compte ?</Text>
+            <TouchableOpacity
+              style={[styles.buttonModale]}
+              onPress={() => {
+                setDisplayModale(false)
+                deleteAccount(props.user_id, props.api_token).then(data => {
+                  ToastAndroid.show(data.message, ToastAndroid.SHORT);
+                  _disconnect()
+                })
+              }
+              }
+            >
+              <Text style={styles.buttonTextModale}>Supprimer mon compte</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+        </Modal>
+      )
+  }
 
 
     return (
         <View>
             <Header navigation={props.navigation}/>
+            {deleteAccountModal}
             <Text>Bienvenue {props.user_name}</Text>
             <Button
             title='Se déconnecter'
@@ -43,7 +75,29 @@ function Profile(props){
 }
 
 const styles = StyleSheet.create({
-    
+  centeredView: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#00000088",
+  },
+  modalView: {
+    backgroundColor: "white",
+    borderRadius: 5,
+    paddingVertical: 30,
+    paddingHorizontal: 60,
+  },
+  buttonModale: {
+    borderRadius: 3,
+    padding: 10,
+    elevation: 2,
+    backgroundColor: "#df4c4c",
+  },
+  buttonTextModale: {
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center"
+  },
   });
 
 const mapStateToProps = (state) => {
